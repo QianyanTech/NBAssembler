296847b0debf0dea5090c7fbd494213c8333ce3a
Alvin Zhu <alvin.zhuge@gmail.com>
2020-09-30 11:23:05 +0800

    v11.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='nbas',
-        version='0.1',
+        version='11.1',
         author='Alvin Zhu @ Kignis',
         author_email='alvin.zhuge@gmail.com',
         packages=['nbas'],
