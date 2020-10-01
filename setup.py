79bdb0a15e3b251f881b7d9b598e0cef6ecce788
Alvin Zhu <alvin.zhuge@gmail.com>
2020-10-01 18:03:32 +0800

    统一版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='nbas',
-        version='11.1',
+        version='11.1.0',
         author='Alvin Zhu @ Kignis',
         author_email='alvin.zhuge@gmail.com',
         packages=['nbas'],
